c82917681e62466c4635868e64f160d76937acb9
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-02-25 10:24:37 +0400

    Added some static class

---
+++ HomeWorkClassesStructs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeWorkClassesStructs
+{
+    internal class Program
+    {   
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Temperature.CelsiusToFahrenheit("30"));
+            Console.WriteLine(Temperature.FahrenheitToCelsius("30"));
+        }
+    }
+}
---
+++ HomeWorkClassesStructs/Temperature.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkClassesStructs
+{
+    static class Temperature
+    {
+        public static double CelsiusToFahrenheit(string temperatureCelsius)
+        {
+            double celsius = double.Parse(temperatureCelsius);
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            return fahrenheit;
+
+        }
+
+        public static double FahrenheitToCelsius(string temperatureFahrenheit)
+        {
+            double farenheit = double.Parse(temperatureFahrenheit);
+
+            double celsius = (farenheit - 32) * 5 / 9;
+
+            return celsius;
+        }
+    }
+}
